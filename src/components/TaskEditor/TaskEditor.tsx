7964e12ce2bb97e97f219ad5f22d8f4d0e9e9685
Mirror <yllikano@gmail.com>
2025-01-12 03:22:29 +0200

    Refactor: remove redundant dispatch from TaskEditor

--- src/components/TaskEditor/TaskEditor.tsx
+++ src/components/TaskEditor/TaskEditor.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import { editTask } from '../../store/slices/tasksSlice';
 import { TaskEditorProps } from '../../types/task-editor-props';
 import styles from './TaskEditor.module.css';
 
 const TaskEditor: React.FC<TaskEditorProps> = ({ taskId, onSave, onCancel }) => {
-  const dispatch = useDispatch();
 
   // Получаем начальное значение для редактируемой задачи из Redux
   const task = useSelector((state: RootState) =>
@@ -35,13 +33,13 @@ const TaskEditor: React.FC<TaskEditorProps> = ({ taskId, onSave, onCancel }) => 
   };
 
   const handleSave = () => {
+    // !newText.trim() означает: "Если строка пустая, включая случай с пробелами, то...".
     if (!newText.trim()) {
       alert('Text cannot be empty');
       return;
     }
 
     if (task) {
-      dispatch(editTask({ id: task.id, newText })); // Делаем dispatch через Redux
       onSave(newText); // Вызываем onSave, передавая новый текст
     }
   };
